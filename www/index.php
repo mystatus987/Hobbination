ee766cb20d43260247bc80acf786d70c746973ee
johannes <jmuljana@gmail.com>
2022-03-04 11:17:02 +1100

    adding database class

--- www/index.php
+++ www/index.php
@@ -1,7 +1,9 @@
 <?php
-echo "hello world";
-// phpinfo();
-$myvar = 20;
-$myname = "Johannes";
-echo $myname . " " . $myvar;
+// echo "hello world";
+// // phpinfo();
+// $myvar = 20;
+// $myname = "Johannes";
+// echo $myname . " " . $myvar;
+$user = getenv("dbuser");
+echo "username is $user";
 ?>
--- www/src/Database.php
+++ www/src/Database.php
@@ -1,17 +1,45 @@
 <?php
 namespace textreview;
 
+use Exception;
+
 class Database {
   private $username;
   private $password;
-  private $host;
-  private $db;
-  // function to set up the class
-  protected function __construct() {
+  private $database_name;
+  private $database_host;
+  private $connection;
 
+  protected function __construct()
+  {
+    $this -> username = getenv("dbuser");
+    $this -> password = getenv("dbpass");
+    $this -> database_name = getenv("dbname");
+    $this -> database_host = getenv("dbhost");
   }
-  protected function getConnection() {
 
+  private function connect() 
+  {
+    try
+    {
+      $this -> connection = mysqli_connect(
+        $this -> database_host,
+        $this -> username,
+        $this -> password,
+        $this -> database_name
+      );
+    }
+    catch( Exception $exception) 
+    {
+      echo $exception -> getMessage();
+    }
+    
+  }
+
+  protected function getConnection()
+  {
+    return $this -> connection;
   }
 }
+
 ?>
